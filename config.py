614f338301105cc34b6173933c4931e33dcf1f1f
Niraj Kumar Singh <nirajkumar11288@gmail.com>
2025-11-09 16:55:23 +0530

    add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+CONFIG = {
+    "HIBP_API_KEY": "your_key_here",
+    "GITHUB_TOKEN": "your_token_here",
+    "SHODAN_API_KEY": "your_key_here",
+    "HUNTER_API_KEY": "your_key_here",
+    "CLEARBIT_KEY": "your_key_here",
+    "FULLCONTACT_KEY": "your_key_here",
+    "URLSCAN_API_KEY": "your_key_here",
+    "IPINFO_TOKEN": "your_key_here",
+    "DARKWEB_KEY": "your_key_here"
+}
